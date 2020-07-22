fe0e2ccb5ae73d69d59128c41d91e1f043d0c563
JRProgramming <ramirezjohnny@icloud.com>
2020-07-22 11:48:30 -0400

    Change method from GET to POST when playing MadLib

--- app.py
+++ app.py
@@ -3,18 +3,21 @@ import requests
 app = Flask(__name__)
 
 responses = []
-madlibAPI = requests.get("http://madlibz.herokuapp.com/api/random?minLength=2")
+madlibAPI = requests.get("http://madlibz.herokuapp.com/api/random")
 questions = madlibAPI.json()["blanks"]
 story = madlibAPI.json()["value"]
 title = madlibAPI.json()["title"]
 form = "<input type='text' name='response'><input type='submit'><br>"
 error = "<br><strong style='color: red;'>Please type something into the textfield</strong>"
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def madlib():
     global questions, responses, story, title, form
-    if request.args.get("response") and len(str(request.args.get("response")).strip()) != 0:
-        responses.append(request.args.get("response"))
+    if request.form.get("restart"):
+        newGame()
+        return render_template("index.html", form=form, partOfSpeech=questions[0])
+    if request.method == "POST" and len(str(request.form.get("response")).strip()) != 0:
+        responses.append(request.form.get("response"))
         if len(responses) == len(questions):
             storyString = ""
             for index, question in enumerate(story):
@@ -27,16 +30,12 @@ def madlib():
                         storyString += question
             return render_template("index.html", title=title, story=storyString)
         return render_template("index.html", form=form, partOfSpeech=questions[len(responses)])
-    elif len(request.args) != 0:
-        return render_template("index.html", form=form, error=error, partOfSpeech=questions[len(responses)])
-    else:
-        newGame()
-        return render_template("index.html", form=form, partOfSpeech=questions[0])
-
+    return render_template("index.html", form=form, error=error, partOfSpeech=questions[len(responses)])
+        
 def newGame():
     global responses, madlibAPI, questions, story, title
     responses = []
-    madlibAPI = requests.get("http://madlibz.herokuapp.com/api/random?minLength=2")
+    madlibAPI = requests.get("http://madlibz.herokuapp.com/api/random")
     questions = madlibAPI.json()["blanks"]
     story = madlibAPI.json()["value"]
     title = madlibAPI.json()["title"]
